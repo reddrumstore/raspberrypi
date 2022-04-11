9ee52ce3e14c57f5e38c2231249113ae5cf2ef95
reddrumstore <74611379+reddrumstore@users.noreply.github.com>
2022-04-11 10:36:01 -0400

    Create setupVi.sh

---
+++ setupVi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+
+#  ver. 05
+#  do initial setup for vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+mkdir ~/.vim/backups
+mkdir ~/.vim/swaps
+mkdir ~/.vim/undo
